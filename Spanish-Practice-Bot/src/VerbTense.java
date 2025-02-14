5de450eaa9125e7b1d74518a79982ec72f9240d6
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2025-02-14 16:45:36 -0500

    adding instance fields for conjugations

--- Spanish-Practice-Bot/src/VerbTense.java
+++ Spanish-Practice-Bot/src/VerbTense.java
@@ -1,4 +1,10 @@
 
 public abstract class VerbTense {
-
+	protected String yo;
+	protected String tu;
+	protected String el_ella_usted;
+	protected String nosotros_as;
+	protected String vosotros_as;
+	protected String ellos_as_ustedes;
+	protected String vos;
 }
