9673b4aaf59b8a4fe58a3e62b80736060b0e91df
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2025-02-15 14:25:49 -0500

    implementing methods for other conjugations

--- Spanish-Practice-Bot/src/PresenteAr.java
+++ Spanish-Practice-Bot/src/PresenteAr.java
@@ -3,7 +3,7 @@ public class PresenteAr extends VerbTense {
 	
 	//construct a regular ar verb in el presente perfecto
 	public PresenteAr(InfinitiveVerb infinitiveForm) throws VerbException {
-			super(infinitiveForm, "o", "as", "a", "amos", "áis", "an", "");
+			super(infinitiveForm, "o", "as", "a", "amos", "áis", "an", "ás");
 			if ((!(infinitiveForm.getVerbEnding().equalsIgnoreCase("ar")))) {
 				throw new VerbException(infinitiveForm);
 			}
@@ -34,37 +34,74 @@ public class PresenteAr extends VerbTense {
 	@Override
 	protected String conjugateTu() {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder reflexiveStart = new StringBuilder("te ");
+		String verbString = new StringBuilder(infinitiveForm.getVerbStem()).append(tu).toString();
+		if (infinitiveForm.getReflexiveFlag()) {
+			return reflexiveStart.append(verbString).toString();
+		}
+		// TODO Auto-generated method stub
+		return verbString;
 	}
 
 	@Override
 	protected String conjugateElEllaUsted() {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder reflexiveStart = new StringBuilder("se ");
+		String verbString = new StringBuilder(infinitiveForm.getVerbStem()).append(el_ella_usted).toString();
+		if (infinitiveForm.getReflexiveFlag()) {
+			return reflexiveStart.append(verbString).toString();
+		}
+		// TODO Auto-generated method stub
+		return verbString;
 	}
 
 	@Override
 	protected String conjugateNosotrosAs() {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder reflexiveStart = new StringBuilder("nos ");
+		String verbString = new StringBuilder(infinitiveForm.getVerbStem()).append(nosotros_as).toString();
+		if (infinitiveForm.getReflexiveFlag()) {
+			return reflexiveStart.append(verbString).toString();
+		}
+		// TODO Auto-generated method stub
+		return verbString;
 	}
 
 	@Override
 	protected String conjugateVosotrosAs() {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder reflexiveStart = new StringBuilder("os ");
+		String verbString = new StringBuilder(infinitiveForm.getVerbStem()).append(vosotros_as).toString();
+		if (infinitiveForm.getReflexiveFlag()) {
+			return reflexiveStart.append(verbString).toString();
+		}
+		// TODO Auto-generated method stub
+		return verbString;
 	}
 
 	@Override
 	protected String conjugateEllosEllasUstedes() {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder reflexiveStart = new StringBuilder("se ");
+		String verbString = new StringBuilder(infinitiveForm.getVerbStem()).append(ellos_as_ustedes).toString();
+		if (infinitiveForm.getReflexiveFlag()) {
+			return reflexiveStart.append(verbString).toString();
+		}
+		// TODO Auto-generated method stub
+		return verbString;
 	}
 
 	@Override
 	protected String conjugateVos() {
 		// TODO Auto-generated method stub
-		return null;
+		// TODO Auto-generated method stub
+				StringBuilder reflexiveStart = new StringBuilder("te ");
+				String verbString = new StringBuilder(infinitiveForm.getVerbStem()).append(vos).toString();
+				if (infinitiveForm.getReflexiveFlag()) {
+					return reflexiveStart.append(verbString).toString();
+				}
+				// TODO Auto-generated method stub
+				return verbString;
 	}
 
 }
